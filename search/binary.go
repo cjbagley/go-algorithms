e17b657063f9730e291602f8c35cbec88ffe2ef6
Colin Bagley <cjbagley@googlemail.com>
2024-06-06 19:46:03 +0100

    fix: Moved binary search to search folder

--- search/binary.go
+++ search/binary.go
@@ -1,4 +1,4 @@
-package sort
+package search
 
 func Binary(s []int, needle int) (bool, int) {
 	if len(s) == 0 {
@@ -6,9 +6,10 @@ func Binary(s []int, needle int) (bool, int) {
 	}
 
 	lowest := 0
-	highest := len(s)
-	for lowest < highest {
-		mid := lowest + ((highest - lowest) / 2)
+	highest := len(s) - 1
+	for lowest <= highest {
+		//mid := lowest + ((highest - lowest) / 2)
+		mid := (lowest + highest) / 2
 		i := s[mid]
 
 		if i == needle {
@@ -18,7 +19,7 @@ func Binary(s []int, needle int) (bool, int) {
 		if needle > i {
 			lowest = mid + 1
 		} else {
-			highest = mid
+			highest = mid - 1
 		}
 	}
 
