14c7ea26564a26a543f447cfa4440704ecabaca1
Colin Bagley <cjbagley@googlemail.com>
2024-06-08 12:00:36 +0100

    feat: Added slice Pop and GetLowest

--- slice/slice.go
+++ slice/slice.go
@@ -1,11 +1,10 @@
 package slice
 
 import (
-	"fmt"
 	"math/rand"
 )
 
-func MakeRandomInts(numItems, max int) []int {
+func MakeRandom(numItems, max int) []int {
 	s := make([]int, numItems)
 	for i := 0; i < numItems; i++ {
 		s[i] = rand.Intn(max)
@@ -25,4 +24,29 @@ func IsSorted(slice []int) bool {
 	}
 
 	return true
+}
+
+func Pop(slice []int, index int) []int {
+	length := len(slice)
+	if length == 0 {
+		return slice
+	}
+
+	if index > length || index < 0 {
+		return slice
+	}
+
+	return append(slice[:index], slice[index+1:]...)
+}
+
+func GetLowest(slice []int) (lowestIndex int) {
+	lowestValue := slice[0]
+	for i := 1; i < len(slice); i++ {
+		if slice[i] < lowestValue {
+			lowestValue = slice[i]
+			lowestIndex = i
+		}
+	}
+
+	return lowestIndex
 }
--- slice/slice_test.go
+++ slice/slice_test.go
@@ -2,6 +2,7 @@ package slice_test
 
 import (
 	"github.com/cjbagley/go-algorithms/slice"
+	"reflect"
 	"testing"
 )
 
@@ -17,10 +18,10 @@ func TestMakeRandomInts(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := slice.MakeRandomInts(tt.numItems, tt.max)
+			got := slice.MakeRandom(tt.numItems, tt.max)
 			length := len(got)
 			if length != tt.numItems {
-				t.Errorf("MakeRandomInts() contains %v items, want %v", length, tt.numItems)
+				t.Errorf("MakeRandom() contains %v items, want %v", length, tt.numItems)
 			}
 
 			first := -1
@@ -35,12 +36,12 @@ func TestMakeRandomInts(t *testing.T) {
 				}
 
 				if v > tt.max {
-					t.Errorf("MakeRandomInts() value %v exceeds max %v", v, tt.max)
+					t.Errorf("MakeRandom() value %v exceeds max %v", v, tt.max)
 				}
 			}
 
 			if duplicate == tt.numItems {
-				t.Errorf("MakeRandomInts() contains only the same int: %v", got)
+				t.Errorf("MakeRandom() contains only the same int: %v", got)
 			}
 		})
 	}
@@ -62,6 +63,47 @@ func TestIsSorted(t *testing.T) {
 			got := slice.IsSorted(tt.slice)
 			if got != tt.want {
 				t.Errorf("IsSorted(%v) returned %v, want %v", tt.slice, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPop(t *testing.T) {
+	tests := []struct {
+		name  string
+		slice []int
+		index int
+		want  []int
+	}{
+		{name: "Pop first index", index: 0, want: []int{8, 2, 1, 8, 2, 0, 10}},
+		{name: "Pop an inner index", index: 3, want: []int{8, 8, 2, 8, 2, 0, 10}},
+		{name: "Pop last index", index: 7, want: []int{8, 8, 2, 1, 8, 2, 0}},
+		{name: "Handle invalid index", index: 10, want: []int{8, 8, 2, 1, 8, 2, 0, 10}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			test := []int{8, 8, 2, 1, 8, 2, 0, 10}
+			if got := slice.Pop(test, tt.index); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Pop() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetLowest(t *testing.T) {
+	tests := []struct {
+		name  string
+		slice []int
+		want  int
+	}{
+		{name: "Lowest is first index", slice: []int{1, 2, 3, 4, 5, 6, 7, 8}, want: 0},
+		{name: "Lowest is inner index", slice: []int{9, 7, 10, 4, 1, 6, 5, 8}, want: 4},
+		{name: "Lowest is last index", slice: []int{8, 7, 6, 5, 4, 3, 2, 1, 0}, want: 8},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotIndex := slice.GetLowest(tt.slice); gotIndex != tt.want {
+				t.Errorf("GetLowest() = %v, want %v", gotIndex, tt.want)
 			}
 		})
 	}
